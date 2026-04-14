6839cc16fd002375a4c909db1cbc601597758ea2
devlucasr <developerlucasribeiro@gmail.com>
2026-04-14 00:31:34 -0300

    add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+    es2022: true,
+  },
+
+  parser: "@typescript-eslint/parser",
+
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+    sourceType: "module",
+  },
+
+  plugins: ["@typescript-eslint"],
+
+  ignorePatterns: [".eslintrc.cjs", "dist", "node_modules"],
+
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+
+  rules: {
+    "no-console": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+  },
+};
