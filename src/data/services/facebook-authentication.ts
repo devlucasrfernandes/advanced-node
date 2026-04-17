c4675572e0d06890743c528a93268d6e4c046499
Lucas Ribeiro Fernandes <lucas.fernandes@teddydigital.io>
2026-04-17 16:29:13 -0300

    move business login to a domain entity

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -5,6 +5,7 @@ import {
 } from '@/data/contracts/apis/repos'
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
+import { FacebookAccount } from '@/domain/models'
 
 export class FacebookAuthenticationService {
   constructor(
@@ -20,13 +21,8 @@ export class FacebookAuthenticationService {
       const accountData = await this.userAccountRepo.load({
         email: fbData.email,
       })
-
-      await this.userAccountRepo.saveWithFacebook({
-        id: accountData?.id,
-        name: accountData?.name ?? fbData.name,
-        email: fbData.email,
-        facebookId: fbData.facebookId,
-      })
+      const fbAccount = new FacebookAccount(fbData, accountData)
+      await this.userAccountRepo.saveWithFacebook(fbAccount)
     }
 
     return new AuthenticationError()
